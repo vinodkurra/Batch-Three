3ece08a9c7ee23bcfa9e1cd18d8a49cfc1f9ad89
vinodkurra <ugandharkurra1996@gmail.com>
2025-05-29 08:06:39 +0530

    Add HTML structure and JavaScript functions for interactive examples, including string methods and data types

---
+++ Javascript/class14/basic.js
@@ -0,0 +1,8 @@
+
+// Here we are updatinh h2 element / COMMENTS
+
+function myFunction() {
+  const h2 = document.getElementById("h2");
+  h2.innerHTML = h2.innerHTML + "World";
+  alert("Hey Your H2 tag updated");
+}

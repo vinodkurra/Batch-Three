73341a70b4d2cea7ee81419b82eb94441f273429
vinodkurra <ugandharkurra1996@gmail.com>
2025-06-13 17:48:46 +0530

    Update variable scope and re-assignment rules for var, let, and const; add fetch and axios API call examples with error handling and sessionStorage usage

---
+++ Javascript/class27/basic.js
@@ -0,0 +1,8 @@
+// // Here we are updatinh h2 element / COMMENTS
+sessionStorage.setItem("name", "vinod");
+sessionStorage.setItem("age", 34);
+sessionStorage.setItem("role", "devloper");
+
+function myFunction() {
+  sessionStorage.clear();
+}
